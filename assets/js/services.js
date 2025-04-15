6d788f16da1b34a41476f92dc93fa14f5c67a5cb
Zarir Engineer <zarir.utube@gmail.com>
2025-04-15 19:56:44 +0530

    update

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,40 +1,30 @@
-// Services
+// Services - Bootstrap Modal Logic
 
 function servicesOpenPopup(element) {
-    var keyword = element.getAttribute("data-keyword");
-    var description = element.getAttribute("data-description");
-    var image = element.getAttribute("data-image");
-    var fulltext = element.getAttribute("data-fulltext");
+    const keyword = element.getAttribute("data-keyword");
+    const description = element.getAttribute("data-description") || "More details coming soon...";
+    const image = element.getAttribute("data-image");
+    const fulltext = element.getAttribute("data-fulltext");
+
     console.log('+++ keyword ', keyword);
     console.log('+++ description ', description);
     console.log('+++ image ', image);
+
     // Update modal content
     document.getElementById("popup-title").innerText = keyword;
-    document.getElementById("popup-description").innerText = description || "More details coming soon...";
-    document.getElementById("popup-modal").style.display = "block";
+    document.getElementById("popup-description").innerText = description;
+    document.getElementById("popup-image").src = image || "";
+    document.getElementById("popup-full-text").innerText = fulltext || "";
 
-    if (image) {
-        document.getElementById("popup-image").style.backgroundImage = "url(" + image + ")";
-    }
-
-    if (fulltext) {
-        document.getElementById("popup-full-text").innerText = fulltext;
-    }
-
-    // Show modal
-    document.getElementById("popup-content").style.display = "block";
-    document.getElementById("overlay").style.display = "block";
-
-    // Prevent background scrolling
-    document.body.classList.add("modal-open");
-
-    document.querySelectorAll(".background-element").forEach(el => el.style.display = "none");
+    // Show Bootstrap modal
+    const popupModal = new bootstrap.Modal(document.getElementById('popup-modal'));
+    popupModal.show();
 }
 
 function servicesClosePopup() {
-    document.getElementById("popup-content").style.display = "none";
-    document.getElementById("overlay").style.display = "none";
-    document.body.classList.remove("modal-open"); // Re-enable scrolling
-    // Enable scrolling
-    document.getElementById("popup-modal").style.display = "none";
+    const modalEl = document.getElementById("popup-modal");
+    const modalInstance = bootstrap.Modal.getInstance(modalEl);
+    if (modalInstance) {
+        modalInstance.hide();
+    }
 }
